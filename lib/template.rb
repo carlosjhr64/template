8721b8ea6decbd3fee35ed89250b6672194e1207
carlosjhr64 <carlosjhr64@gmail.com>
2019-12-22 15:23:46 -0800

    project template initial commit

---
+++ lib/template.rb
@@ -0,0 +1,6 @@
+module TEMPLATE
+  VERSION = '0.0.0'
+  require 'template/template'
+end
+# Requires:
+# `ruby`
---
+++ lib/template/template.rb
@@ -0,0 +1,11 @@
+module TEMPLATE
+  def self.main
+    Template.new.main
+  end
+
+  class Template
+    def main
+      puts "OK"
+    end
+  end
+end
